11a3ff425b0bed301dd944cdfa309808fc005a26
Chae-space <90403366+Chae-space@users.noreply.github.com>
2022-09-27 02:36:22 +0900

    Create ReplyMapper.java

---
+++ src/Reply/ReplyMapper.java
@@ -0,0 +1,23 @@
+package com.chae.SimpleBoard.Reply;
+
+import java.util.ArrayList;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ReplyMapper {
+
+	void insert(Reply r);
+
+	Reply select(int reply_num);
+
+	ArrayList<Reply> selectListByBoard_num(int board_num);
+
+	ArrayList<Reply> selectListByParent_reply_num(int parent_reply_num);
+
+	void update(Reply r);
+
+	void delete(int reply_num);
+
+	int selectSeqCurrval();
+}
